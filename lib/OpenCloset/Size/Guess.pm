619202a946deeb76c0e909a0a7c6e83ed33a4997
Keedi Kim <keedi.k@gmail.com>
2015-07-21 16:43:36 +0900

    Ready to dzil

---
+++ lib/OpenCloset/Size/Guess.pm
@@ -0,0 +1,23 @@
+package OpenCloset::Size::Guess;
+# ABSTRACT: OpenCloset size guess module
+
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+    ...
+
+
+=head1 DESCRIPTION
+
+...
